5104476120408710c84064128816c9fb09676269
Hackbright Student <info@hackbrightacademy.com>
2015-07-06 17:20:29 -0700

    Used a large text file to count the words in it

--- wordcount.py
+++ wordcount.py
@@ -1,6 +1,6 @@
  # put your code here.
  
-fhandle = open("test.txt")
+fhandle = open("twain.txt")
 word_count = {}
 
 for line in fhandle:
@@ -9,15 +9,7 @@ for line in fhandle:
    
     for word in word_list:
         word_count[word] = word_count.get(word, 0) + 1
-        
-
-
-
-
-#       if word in word_count:
-#            word_count[word] = word_count[word] + 1
-#        else:
-#            word_count[word] = 1  
+          
 
 for word, count in word_count.items():
     print "%s %d" % (word, count) 
